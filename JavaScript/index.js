47b85e171caaa73518bb3b8e80d7a7d6099b9b95
supreeth10 <smuddu2@pdx.edu>
2022-11-23 00:25:25 -0800

    created the basic layout for resume, Projects and contact page

---
+++ JavaScript/index.js
@@ -0,0 +1,9 @@
+document.getElementById("button_db").addEventListener("click", () => {
+  console.log("clicked");
+  location.replace("https://github.com/Supreeth10");
+});
+
+document.getElementById("button_se").addEventListener("click", () => {
+  console.log("clicked");
+  location.replace("https://github.com/Supreeth10");
+});
