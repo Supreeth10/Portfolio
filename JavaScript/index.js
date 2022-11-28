223983c6b4876f21f3a6e990eb284e9e0a574c50
supreeth10 <smuddu2@pdx.edu>
2022-11-28 13:51:11 -0800

    Small bug fixes

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -19,12 +19,12 @@ let button_db = document
   .getElementById("button_db")
   .addEventListener("click", () => {
     console.log("clicked");
-    location.replace("https://github.com/Supreeth10");
+    location.replace("https://github.com/Supreeth10/E-Commerce-Database");
   });
 
 let button_se = document
   .getElementById("button_se")
   .addEventListener("click", () => {
     console.log("clicked");
-    location.replace("https://github.com/Supreeth10");
+    location.replace("https://github.com/Supreeth10/Click-n-Split");
   });
