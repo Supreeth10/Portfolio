e1a9f68c2b24f0aa46e1a01715b801613da96960
supreeth10 <smuddu2@pdx.edu>
2022-11-28 14:21:47 -0800

    rearraged js code into 2 new files

--- JavaScript/contact.js
+++ JavaScript/contact.js
@@ -14,17 +14,3 @@ form.addEventListener("submit", (e) => {
     console.warn("You must fill all fields to submit this form!");
   }
 });
-
-let button_db = document
-  .getElementById("button_db")
-  .addEventListener("click", () => {
-    console.log("clicked");
-    location.replace("https://github.com/Supreeth10/E-Commerce-Database");
-  });
-
-let button_se = document
-  .getElementById("button_se")
-  .addEventListener("click", () => {
-    console.log("clicked");
-    location.replace("https://github.com/Supreeth10/Click-n-Split");
-  });
---
+++ JavaScript/projects.js
@@ -0,0 +1,13 @@
+let button_db = document
+  .getElementById("button_db")
+  .addEventListener("click", () => {
+    console.log("clicked");
+    location.replace("https://github.com/Supreeth10/E-Commerce-Database");
+  });
+
+let button_se = document
+  .getElementById("button_se")
+  .addEventListener("click", () => {
+    console.log("clicked");
+    location.replace("https://github.com/Supreeth10/Click-n-Split");
+  });
