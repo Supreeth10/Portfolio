b3fa069d38b882ff7019d733bff965eec31a1dc3
supreeth10 <smuddu2@pdx.edu>
2022-11-28 01:54:27 -0800

    made minor changes to all pages. Added validation to contact form.Updated the Bio's in the about and resume page

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,9 +1,34 @@
-document.getElementById("button_db").addEventListener("click", () => {
-  console.log("clicked");
-  location.replace("https://github.com/Supreeth10");
+let submit_button = document.getElementById("submit_button");
+let Name = document.getElementById("username");
+let email = document.getElementById("InputEmail");
+let msg = document.getElementById("message");
+let form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let Name = document.getElementById("username");
+  let email = document.getElementById("InputEmail");
+  let msg = document.getElementById("message");
+  if (
+    Name.value.length == 0 &&
+    email.value.length == 0 &&
+    msg.value.length == 0
+  ) {
+    alert("Can not submit an empty form");
+    console.warn("You must fill all fields to submit this form!");
+  }
 });
 
-document.getElementById("button_se").addEventListener("click", () => {
-  console.log("clicked");
-  location.replace("https://github.com/Supreeth10");
-});
+let button_db = document
+  .getElementById("button_db")
+  .addEventListener("click", () => {
+    console.log("clicked");
+    location.replace("https://github.com/Supreeth10");
+  });
+
+let button_se = document
+  .getElementById("button_se")
+  .addEventListener("click", () => {
+    console.log("clicked");
+    location.replace("https://github.com/Supreeth10");
+  });
